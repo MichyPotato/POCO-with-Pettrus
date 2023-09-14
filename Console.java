1a7d174514ad91c02dc92f8a17dcf210f0761d52
pettruskonnoth <pettrusk@gmail.com>
2023-09-14 15:14:43 -0400

    defult , parTIAL, and full constructor

--- Console.java
+++ Console.java
@@ -6,4 +6,33 @@ public class Console {
     private int energyConsumption;
     private boolean hasColorDisplay;
     private Brand brand;
+
+    //defult constructor
+    public Console(){
+        this.name = "Oculus Quest 2
+        this.storage = 0;
+        this.energyConsumption = 0;
+        this.hasColorDisplay = false;
+        this.brand = Brand.META;
+    }
+    //partial constructor
+    public Console(String name, double storage, int energyConsumption){
+        this.name = name;
+        this.storage = storage;
+        this.energyConsumption = energyConsumption;
+        this.hasColorDisplay = false;
+        this.brand = Brand.META;
+    }
+    //full constructor
+    public Console(String name, double storage, int energyConsumption, boolean hasColorDisplay, Brand brand){
+        this.name = name;
+        this.storage = storage;
+        this.energyConsumption = energyConsumption;
+        this.hasColorDisplay = hasColorDisplay;
+        this.brand = brand;
+    }
+
+
+
 }
+
