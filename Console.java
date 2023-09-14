80e86c40956dfb0606d32ce095c19d38956764c0
MichyPotato <96063090+MichyPotato@users.noreply.github.com>
2023-09-14 19:15:10 +0000

    added 3 more constructors

--- Console.java
+++ Console.java
@@ -2,14 +2,14 @@
 
 public class Console {
     private String name;
+    private Brand brand;
     private double storage;
     private int energyConsumption;
     private boolean hasColorDisplay;
-    private Brand brand;
 
-    //defult constructor
+//defult constructor
     public Console(){
-        this.name = "Oculus Quest 2
+        this.name = "Oculus Quest 2";
         this.storage = 0;
         this.energyConsumption = 0;
         this.hasColorDisplay = false;
@@ -32,7 +32,28 @@ public class Console {
         this.brand = brand;
     }
 
-
+    
+    public Console(String name, Brand brand, double storage){
+        this.name =  name;
+        this.brand = brand;
+        this.storage = storage;
+        this.energyConsumption = 0;
+        this.hasColorDisplay = true;
+    }
+    public Console(String name, Brand brand, double storage, int energyConsumption){
+        this.name =  name;
+        this.brand = brand;
+        this.storage = storage;
+        this.energyConsumption = energyConsumption;
+        this.hasColorDisplay = true;
+    }
+    public Console(String name, Brand brand, double storage, int energyConsumption, boolean hasColorDisplay){
+        this.name =  name;
+        this.brand = brand;
+        this.storage = storage;
+        this.energyConsumption = energyConsumption;
+        this.hasColorDisplay = hasColorDisplay;
+    }
 
 }
 
